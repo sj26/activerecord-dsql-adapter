65212568413e342d5cf38bb05483078a8dceec53
Samuel Cochran <sj26@sj26.com>
2024-12-04 17:15:02 +1100

    Use lazy load hooks to register adapter

--- lib/activerecord-dsql-adapter.rb
+++ lib/activerecord-dsql-adapter.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-require "active_record"
-require "active_record/connection_adapters"
+require "active_support/lazy_load_hooks"
 
-ActiveRecord::ConnectionAdapters.register("dsql", "ActiveRecord::ConnectionAdapters::DSQLAdapter", "active_record/connection_adapters/dsql_adapter")
+ActiveSupport.on_load(:active_record) do
+  ActiveRecord::ConnectionAdapters.register("dsql", "ActiveRecord::ConnectionAdapters::DSQLAdapter", "active_record/connection_adapters/dsql_adapter")
+end
